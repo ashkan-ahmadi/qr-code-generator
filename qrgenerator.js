94761f75d4969ea81e6ab6ab264cca2f580e567f
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-06 11:21:03 +0200

    Update generateQR function

--- qrgenerator.js
+++ qrgenerator.js
@@ -6,12 +6,12 @@ const data = XLSX.utils.sheet_to_json(ws)
 
 const generateQR = (filename, data, options = {}) => {
   try {
-    QR.toFile(`exported_qr_codes/${filename}.png`, data, options)
+    QR.toFile(`exported_qr_codes/${filename}.png`, data.toString(), options)
   } catch (error) {
     console.error(error)
   }
 }
 
 data.forEach(row => {
-  generateQR(row.TICKETS, row.TICKETS + '', { width: 250 }) // the + '' part converts numbers to strings. otherwise, it wont work. the value MUST BE STRING
+  generateQR(row.TICKETS, row.TICKETS, { width: 250 })
 })
