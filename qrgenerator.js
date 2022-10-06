2a0dcf8033dd45a4ed043747fb93a7b8d78712b9
Ashkan Ahmadi <ashkan.ahmadi88@gmail.com>
2022-10-06 12:42:09 +0200

    Add default settings to generateQR function

--- qrgenerator.js
+++ qrgenerator.js
@@ -4,14 +4,23 @@ const XLSX = require('xlsx')
 const ws = XLSX.readFile('tickets.xlsx').Sheets['Sheet1']
 const data = XLSX.utils.sheet_to_json(ws)
 
-const generateQR = async (filename, data, options = {}) => {
+const generateQR = (filename, data, options = {}) => {
+  const defaultOptions = {
+    // see complete list with defaults: https://github.com/soldair/node-qrcode#qr-code-options
+    width: 250, // px
+    margin: 2, // default: 4
+    type: 'png', // string png|svg|utf8, default: png
+  }
+
+  const settings = { ...defaultOptions, ...options }
+
   try {
-    await QR.toFile(`exported_qr_codes/${filename}.png`, data.toString(), options)
+    QR.toFile(`exported_qr_codes/${filename}.png`, data.toString(), settings)
   } catch (error) {
     console.error(error)
   }
 }
 
 data.forEach(row => {
-  generateQR(row.TICKETS, row.TICKETS, { width: 250 })
+  generateQR(row.TICKETS, row.TICKETS)
 })
